ecb637fd425ac1e2e9b737f99a5c23dd1dbeaa39
ismail885 <115149488+ismail885@users.noreply.github.com>
2025-02-13 13:02:41 +0100

    Delete src directory

--- src/components/Header.js
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-blue-600 p-4 flex justify-between text-white">
-      <h1 className="text-2xl font-bold">Kosupure</h1>
-    </header>
-  );
-}
--- src/components/Tweet.js
+++
@@ -1,9 +0,0 @@
-export default function Tweet({ user, content, createdAt }) {
-    return (
-      <div className="border p-4 rounded-lg shadow-md mb-4">
-        <h2 className="font-bold">{user}</h2>
-        <p>{content}</p>
-        <span className="text-sm text-gray-500">{createdAt}</span>
-      </div>
-    );
-}
